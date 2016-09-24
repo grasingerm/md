e6341605dd52848cb26a3ef9670adf899de98ae0
Matthew Grasinger <grasingerm@gmail.com>
2016-09-24 04:16:14 -0400

    potential energy interface

--- hw2/include/potentials.hpp
+++ hw2/include/potentials.hpp
@@ -1,16 +1,35 @@
 #ifndef __POTENTIALS_HPP__
 #define __POTENTIALS_HPP__
 
-#include <array>
-#include <functional>
+#include <armadillo>
+#include <vector>
 
 namespace mmd {
 
-template <size_t D> double pot_spring(const std::array& r);
-template <size_t D> double pot_LJ(const std::array& r1, const std::array& r2);
-
-template <size_t D>
-class spring_potential {
+//! Abstract base class for potentials
+//!
+//! Defines a public interface for all potentials (e.g. spring, LJ, bond-angle,
+//! etc.)
+class abstract_potential {
+public:
+  //! Calculates total potential energy of the current configuration
+  //!
+  //! 
+  inline double potential_energy(const std::vector<molecular_id>& molecular_ids, 
+                                 arma::mat& positions) {
+    return _potential_energy(molecular_ids, positions);
+  }
+  
+  //! Calculates total potential energy of the current configuration
+  //!
+  //! 
+  inline void update_forces(const std::vector<molecular_id>& molecular_ids, 
+                            arma::mat& positions, arma::mat& forces) {
+    _update_forces(molecular_ids, positions, forces);
+  }
+private:
+  virtual double _potential_energy(const std::vector&, arma::mat&, arma::mat&)=0;
+  virtual void _update_forces(const std::vector&, arma::mat&, arma::mat&)=0;
 };
 
 }
